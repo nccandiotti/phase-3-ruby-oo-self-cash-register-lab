e257b48142f06d8ca5e090fcdcaeeba0793b85d7
Nicole Candiotti <nccandiotti@gmail.com>
2022-02-24 14:58:40 -0500

    commit tree b0b3de08983fe31453370a1da78c6737a7ac7083

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,27 @@
 
+class CashRegister
+    attr_accessor :total, :discount, :item, :price
+
+    def initialize(discount=0)
+        @total = 0
+        @discount = discount
+    end
+
+    def total
+        @total
+    end
+
+    def apply_discount
+        case discount
+        when discount > 0
+            updated total = self.total = total - total*discount
+            updated total
+        else "No discount to apply"
+        end
+    end
+
+    def add_item(item, price, quanitity)
+        price = item*quantity
+        self.total += price
+    end
+end
